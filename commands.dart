e26211cdac39d8accaf203f445ba539f3bcbcc1b
KimOliverManga3 <kimoliver.manga@unc.edu.ph>
2022-04-15 02:27:06 +0800

    Fixed Input Bugs

--- commands.dart
+++ commands.dart
@@ -15,24 +15,63 @@ class Testing{
 
   withdraw(){
     var amnt, amountWithdrawn;
-
-    stdout.write("Please Enter the Amount to be Withdrawn: ");
-    amnt = stdin.readLineSync();
-    amountWithdrawn = int.parse(amnt);
-    withdrawBal = amountWithdrawn;
-    printing("$amountWithdrawn has been Withrawn Successfully.");
-    updating();
+    bool valid = false;
+      try{
+        do{
+          stdout.write("Please Enter the Amount to be Withdrawn: ");
+          amnt = stdin.readLineSync();
+          amountWithdrawn = int.parse(amnt);
+          if(amountWithdrawn > balance || amountWithdrawn <= 0){
+            print("\n-Please Enter a Valid Amount-\n");
+            continue;
+          }
+          else{
+            withdrawBal = amountWithdrawn;
+            printing("$amountWithdrawn has been Withrawn Successfully.");
+            updating();
+            valid = true;
+          }   
+        }while(valid == false);
+      }
+      on FormatException catch(e){
+        print("\n-Invalid Input. Please Try Again-\n");
+        withdraw();
+      }
+      catch(e){
+        print(e);
+      }
+    
   }
 
   deposit(){
     var amnt, amountDeposited;
+    bool valid = false;
 
-    stdout.write("Please Enter the Amount to be Deposited: ");
-    amnt = stdin.readLineSync();
-    amountDeposited = int.parse(amnt);
-    depositBal = amountDeposited;
-    printing("$amountDeposited has been Deposited Successfully.");  
-    updating();
+      try{
+        do{
+          stdout.write("Please Enter the Amount to be Deposited: ");
+          amnt = stdin.readLineSync();
+          amountDeposited = int.parse(amnt);
+          if(amountDeposited <= 0){
+            print("\n-Please Enter a Valid Amount-\n");
+            continue;
+          }
+          else{
+            depositBal = amountDeposited;
+            printing("$amountDeposited has been Deposited Successfully.");  
+            updating();
+            valid = true;
+          }   
+        }while(valid == false);
+      }
+      on FormatException catch(e){
+        print("\n-Invalid Input. Please Try Again-\n");
+        deposit();
+      }
+      catch(e){
+        print(e);
+      }
+    
   }
 
   printing(String line) => print("""
